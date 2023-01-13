9e8f79b13c4a0503ab3d2aabc576c6b2aa25913b
adeline721933 <122544047+adeline721933@users.noreply.github.com>
2023-01-13 09:54:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Cow)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.showArrow(ArrowNames.North)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Angry)
+    basic.showNumber(35.66 * 345.66)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (true) {
+        music.playMelody("F D G C5 C A E B ", 427)
+    }
 })
 basic.showIcon(IconNames.Pitchfork)
