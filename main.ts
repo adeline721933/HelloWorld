37f5bc27dbef735bfbb459f4c3b05dc881d30b46
adeline721933 <122544047+adeline721933@users.noreply.github.com>
2023-01-13 09:35:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Cow)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Angry)
+})
+basic.showIcon(IconNames.Pitchfork)
